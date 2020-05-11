c9ae546d831e6c41baeaf974f65ea7d7af05fb20
Kevin Gobel <kgsea175@gmail.com>
2020-05-11 23:19:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = ""
+var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello," + specialGuest + "!" ;
+var greetSpecialGuest = "Hello," + specialGuest + "!";
 
 var topic = "space";
 
